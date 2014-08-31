8336128f77a28a9e6a453b1550801b2dc4447a21
Justin Maxwell <a@aw2basc.com>
2014-08-30 20:56:21 -0500

    dotfiles

--- dotfiles.js
+++ dotfiles.js
@@ -1,19 +1,21 @@
 var fs = require('fs'),
     path = require('path'),
     spawn = require('child_process').spawn,
-    home = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'],
+    iswin = process.platform === 'win32',
+    home = process.env[iswin ? 'USERPROFILE' : 'HOME'],
+    dot = iswin ? '_' : '.',
     dotfiles = ['vimrc'];
 
 dotfiles.forEach(function(dotfile){
     var curfile = path.join(__dirname, dotfile),
-        destfile = path.join(home, '.' + dotfile),
+        destfile = path.join(home, dot + dotfile),
         bak = destfile + Date.now(),
         link = function(e){
             if(e)throw e;
-            fs.symlink(curfile, destfile);
+            fs.symlink(curfile, destfile, function(e){if(e)throw e;});
         };
-    fs.lstat(destfile, function(err, stats){
-        if(err)link(); 
+    fs.lstat(destfile,function(err, stats){
+        if(err)link();
         else if(stats.isFile())
             fs.rename(destfile, bak, link);
         else if(stats.isSymbolicLink())
