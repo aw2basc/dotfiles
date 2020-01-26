135dd20f6a3f6548c36f6f210f295459c8937033
aw2basc <a@aw2basc.com>
2020-01-26 14:45:24 -0600

    gitignore

--- dotfiles.js
+++ dotfiles.js
@@ -1,10 +1,12 @@
 var fs = require('fs'),
+    { exec } = require('child_process'),
     path = require('path'),
     iswin = process.platform === 'win32',
     home = process.env[iswin ? 'USERPROFILE' : 'HOME'],
     dot = iswin ? '_' : '.',
-    dotfiles = ['vimrc','jshintrc','zshrc'];
+    dotfiles = ['vimrc','jshintrc','zshrc','gitignore_global'];
 
+console.log("dotfiles:");
 dotfiles.forEach(function(dotfile){
     var curfile = path.join(__dirname, dotfile),
         destfile = path.join(home, dot + dotfile),
@@ -12,12 +14,27 @@ dotfiles.forEach(function(dotfile){
         link = function(e){
             if(e)throw e;
             fs.symlink(curfile, destfile, function(e){if(e)throw e;});
+            console.log("linked:" + curfile + "->" + destfile); 
         };
     fs.lstat(destfile,function(e,stats){
-        if(e)link();
-        else if(stats.isFile())
+        if(e){
+          link();
+        }else if(stats.isFile()){
+            console.log("renamed:" + destfile);
             fs.rename(destfile, bak, link);
-        else if(stats.isSymbolicLink())
+        }else if(stats.isSymbolicLink()){
+            console.log("unlinked:" + destfile);
             fs.unlink(destfile, link);
+        }
     });
 });
+if (!iswin) {
+  exec("git config --global core.excludesfile ~/.gitignore_global", (e,stdo,stde) => {
+    if (e || stdo || stde){
+      console.log(e + stde + stdo);
+    }
+    exec("git config --global --list", (e, stdo, stde) => {
+      console.log("\ngit config:\n" + stdo);
+    });
+  });
+}
